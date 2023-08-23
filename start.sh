a949477fda8980c28a3e20d0655c47f95f9be5f1
Will <will_forster@hotmailcom>
2023-08-23 18:50:06 +0200

    Implement pre-exist checks

--- start.sh
+++ start.sh
@@ -6,11 +6,42 @@ img="Ubuntu2204"
 size="Standard_B1s"
 admin="azureuser"
 
-az vm create -n $vm -g $rg --image $img --size $size --admin-username $admin --generate-ssh-keys 
+#Check is resource group exists
+if ! az group show -n $rg &>/dev/null; then
+	#create resource group
+	az group create -n $rg && echo "resource group $rg created"
+else
+        echo "Resource Group $rg already exists"
+fi
 
-az vm open-port --port 80 -n $vm -g $rg
 
-az vm run-command invoke -g $rg -n $vm --command-id RunShellScript --scripts "sudo apt update && sudo apt install -y apache2 mysql-server php libapache2-mod-php php-mysql"
+#check if vm exists
+if ! az vm show -n $vm -g $rg &>/dev/null; then
 
-ssh $admin@$(az vm show -g $rg -n $vm --show-details --query "publicIps" --output tsv) 
+	az vm create -n $vm -g $rg --image $img --size $size --admin-username $admin --generate-ssh-keys && echo "virtual machine $vm sucessfully created"
+else
+	echo "Virtual Machine $vm already exists"
+fi
 
+
+
+publicIP=$(az vm show -g $rg -n $vm --show-details --query "publicIps" --output tsv)
+echo $publicIP
+
+
+# Check if the components are installed
+if ! ssh $admin@$publicIP "which apache2 mysql php"; then
+    # If any component is missing, run the installation command
+    az vm run-command invoke -g $rg -n $vm --command-id RunShellScript --scripts "sudo apt update && sudo apt install -y apache2 mysql-server php libapache2-mod-php php-mysql"
+else
+    echo "Apache, MySQL, and PHP are already installed."
+fi
+
+
+#check is port 80 is open
+if ! curl -I http://$publicIP:80 &>/dev/null; then
+
+	az vm open-port --port 80 -n $vm -g $rg && echo "Port 80 openned"
+else
+        echo "Port 80 already open"
+fi
